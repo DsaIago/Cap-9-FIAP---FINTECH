6c007ebcac9f75b8dbda7875e42c13dd31855a42
Iago Almeida <iagoalmeida429@gmail.com>
2024-12-14 23:02:59 -0300

    fintech alivia

---
+++ src/assets/js/dashboard.js
@@ -0,0 +1,11 @@
+
+const vermais = document.querySelector(".ocultar");
+
+const filtrar = function(){
+
+
+    vermais.style.display === "none" ? vermais.style.display = "block": vermais.style.display = "none";
+
+
+}
+
